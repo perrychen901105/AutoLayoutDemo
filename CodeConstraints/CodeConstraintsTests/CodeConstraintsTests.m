6a106cc4460ae5abfc1f6e5c7e73483d3a45c03e
Perry <chenzhipeng901105@gmail.com>
2014-07-03 15:59:45 +0800

    add code constraint project

---
+++ CodeConstraints/CodeConstraintsTests/CodeConstraintsTests.m
@@ -0,0 +1,34 @@
+//
+//  CodeConstraintsTests.m
+//  CodeConstraintsTests
+//
+//  Created by Perry on 14-7-3.
+//  Copyright (c) 2014年 Perrychen. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface CodeConstraintsTests : XCTestCase
+
+@end
+
+@implementation CodeConstraintsTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
