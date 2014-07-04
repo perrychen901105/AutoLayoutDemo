5c4675044761fb7479f28a3c5ba1239c9dfa8484
Perry <chenzhipeng901105@gmail.com>
2014-07-04 15:23:17 +0800

    delete view controller

--- CustomView/CustomView/AppDelegate.m
+++ CustomView/CustomView/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+#import "ViewController.h"
 
 @implementation AppDelegate
 
@@ -15,6 +16,8 @@
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     // Override point for customization after application launch.
     self.window.backgroundColor = [UIColor whiteColor];
+    ViewController *vc = [[ViewController alloc] init];
+    self.window.rootViewController = vc;
     [self.window makeKeyAndVisible];
     return YES;
 }
